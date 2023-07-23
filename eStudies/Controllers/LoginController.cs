e73d8606988bdc1023ac8691b0a25d2bf5710688
Rexisjojo <128319987+Rexisjojo@users.noreply.github.com>
2023-07-24 00:15:14 +0500

    beforeEmailwork

---
+++ eStudies/Controllers/LoginController.cs
@@ -0,0 +1,59 @@
+﻿using eStudies.Data;
+using eStudies.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eStudies.Controllers
+{
+    public class LoginController : Controller
+    {
+        eStudiesDbContext db;
+        public LoginController(eStudiesDbContext dbc)
+        {
+            db = dbc;
+        }
+
+        //Get
+        [HttpGet]
+        public IActionResult ULogin()
+        {
+            return View();
+        }
+
+        //Post
+
+        [HttpPost]
+        public IActionResult ULogin(Register r)
+        {
+            var x = db.Registers.Where(a => a.UserEmail.Equals(r.UserEmail) && a.Password.Equals(r.Password)).SingleOrDefault();
+
+            if (x != null)
+            {
+                if (x.UserTypeId == 1)
+                {
+                    //RedirectToAction("Index", "Home");  
+                    ViewBag.u = x.UserName;
+                    ViewBag.m = "welcome Student";
+                }
+
+                if (x.UserTypeId == 2)
+                {
+                    //RedirectToAction("Index", "Home");  
+                    ViewBag.u = x.UserName;
+                    ViewBag.m = "welcome Teacher";
+                }
+
+                if (x.UserTypeId == 3)
+                {
+                    //RedirectToAction("Index", "Home");  
+                    ViewBag.u = x.UserName;
+                    ViewBag.m = "welcome Parent/Guardian";
+                }
+            }
+            else
+            {
+                ViewBag.m = "ERROR";
+            }
+            return View();
+        }
+    }
+}
